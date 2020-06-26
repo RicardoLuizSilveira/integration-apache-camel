a2f1af76951a1f63b386e9234653dd6308dad8a5
Ricardo Luiz Silveira <ricardo.silveira@matera.com>
2020-06-25 21:51:32 -0300

    Filter - navegando na estrutura de um XML

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -14,8 +14,11 @@ public class RotaPedidos {
 			@Override
 			public void configure() throws Exception {
 				from("file:pedidos?delay=5s&noop=true").
+					filter().
+						xpath("/pedido/itens/item/formato[text()='EBOOK']").
 					log("${id}").
-					marshal().xmljson().
+					marshal()
+						.xmljson().
 					log("${body}").
 					setHeader("CamelFileName", simple("${file:name.noext}.json")).
 				to("file:saida");
