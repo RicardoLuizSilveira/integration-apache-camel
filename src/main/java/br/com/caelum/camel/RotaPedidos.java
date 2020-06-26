a62ea856863d6ec90cfc8f064c338ea4c20026d1
Ricardo Luiz Silveira <ricardo.silveira@matera.com>
2020-06-25 22:11:31 -0300

    Split

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -14,12 +14,14 @@ public class RotaPedidos {
 			@Override
 			public void configure() throws Exception {
 				from("file:pedidos?delay=5s&noop=true").
+					split().
+						xpath("/pedido/itens/item").
 					filter().
-						xpath("/pedido/itens/item/formato[text()='EBOOK']").
+						xpath("/item/formato[text()='EBOOK']").
+						log("${body}").
 					log("${id}").
 					marshal()
 						.xmljson().
-					log("${body}").
 					setHeader("CamelFileName", simple("${file:name.noext}.json")).
 				to("file:saida");
 				
